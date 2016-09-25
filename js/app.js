dc2c8d5039984357a6fa3ff1b56b9021507f149a
jdesign2014 <jdesign@Mac.local>
2016-09-25 14:21:24 +0200

    las acciones para el desplegar de los menos y el movimiento de los granates

---
+++ js/app.js
@@ -0,0 +1,49 @@
+jQuery(document).ready(function($){
+
+	$('#header__handler').on('click', function(){
+		/* Slider por css */
+		//$('nav').toggleClass('displayed');
+		
+		$('nav').slideDown(400);
+	});
+
+	$('#icon-close').on('click', function(){
+		$('nav').slideUp('400');
+	});
+	
+		/* MOVER LOS GRANAJES A LA VEZ DANDO A EXITO */
+
+	$("#exito").hover(function(){
+		$(".exito").toggleClass("girar");
+		$(".exito_reverse").toggleClass("girar_reverse");
+		}, function(){
+			$(".exito").removeClass("girar");
+			$(".exito_reverse").removeClass("girar_reverse");
+		});
+
+		/* VER SUB-MENU FOOTER */
+
+	$('#js-mapa-web').on('click', function(){
+		$(this).toggleClass('icon-circle-up');
+		$('.mapa--web--displayed').slideToggle(400);
+
+	});
+
+	$('#js-last-works').on('click', function(){
+		$('#js-last-works').toggleClass('icon-circle-up');
+		$('.footer-last-works-displayed').slideToggle(400);
+
+	});
+	$('#js-rrss').on('click', function(){
+		$('#js-rrss').toggleClass('icon-circle-up');
+		$('.footer-rrss-displayed').slideToggle(400);
+
+	});
+	$('#js-contactanos').on('click', function(){
+		$('#js-contactanos').toggleClass('icon-circle-up');
+		$('.footer-contactanos-displayed').slideToggle(400);
+
+	});
+	
+	
+});
